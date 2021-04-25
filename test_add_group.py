39f2efa7283c1f25eab105b2148d1697a1a9f901
ekaterina.lisovets <lisovets_ekaterina@mail.ru>
2021-04-25 13:31:54 +0300

    Задание №1. Тест успешно запускается из консоли и из PyCharm

--- test_add_group.py
+++ test_add_group.py
@@ -7,12 +7,14 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
 
-class TestAddGroup(unittest.TestCase):
+
+class TestAddGroup2(unittest.TestCase):
     def setUp(self):
-        self.wd = webdriver.Firefox()
+        ##self.wd = webdriver.Chrome(executable_path=r'C:\WebDrivers\chromedriver.exe')
+        self.wd = webdriver.Firefox(executable_path=r'C:\Windows\SysWOW64\geckodriver.exe')
         self.wd.implicitly_wait(30)
 
-    def test_add_group(self):
+    def test_add_group2(self):
         wd = self.wd
         wd.get("http://localhost:8080/addressbook/")
         wd.find_element_by_name("user").click()
@@ -26,25 +28,29 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("Group_name")
+        wd.find_element_by_name("group_name").send_keys("name")
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("Group_header")
+        wd.find_element_by_name("group_header").send_keys("header")
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("Group_footer")
+        wd.find_element_by_name("group_footer").send_keys("footer")
         wd.find_element_by_name("submit").click()
         wd.find_element_by_link_text("group page").click()
         wd.find_element_by_link_text("Logout").click()
 
     def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
+        try:
+            self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
         return True
 
     def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
+        try:
+            self.wd.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
         return True
 
     def tearDown(self):

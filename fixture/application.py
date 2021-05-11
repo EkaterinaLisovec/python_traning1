9478c79e11f62668f6d1c3c33f45be399af85fd5
ekaterina.lisovets <lisovets_ekaterina@mail.ru>
2021-05-11 10:09:48 +0300

    Переход на двухуровневую архитектуру - Группы

---
+++ fixture/application.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from fixture.session import SessionHelper
+from fixture.group import GroupHelper
+
+class Application:
+
+    def __init__(self):
+        ##self.wd = webdriver.Chrome(executable_path=r'C:\WebDrivers\chromedriver.exe')
+        self.wd = webdriver.Firefox(executable_path=r'C:\Windows\SysWOW64\geckodriver.exe')
+        self.wd.implicitly_wait(30)
+        self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get("http://localhost:8080/addressbook/")
+
+    def destroy(self):
+        self.wd.quit()

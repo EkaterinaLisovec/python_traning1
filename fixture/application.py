ebcb5376466888d9de6fb3262bf6daae7f19a9f6
ekaterina.lisovets <lisovets_ekaterina@mail.ru>
2021-05-11 11:19:22 +0300

    Создана общая для всех тестов фикстура в файле conftest.py

--- fixture/application.py
+++ fixture/application.py
@@ -6,8 +6,8 @@ from fixture.contact import ContactHelper
 class Application:
 
     def __init__(self):
-        ##self.wd = webdriver.Chrome(executable_path=r'C:\WebDrivers\chromedriver.exe')
-        self.wd = webdriver.Firefox(executable_path=r'C:\Windows\SysWOW64\geckodriver.exe')
+        self.wd = webdriver.Chrome(executable_path=r'C:\WebDrivers\chromedriver.exe')
+        ##self.wd = webdriver.Firefox(executable_path=r'C:\Windows\SysWOW64\geckodriver.exe')
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
--- fixture/group.py
+++ fixture/group.py
@@ -3,9 +3,9 @@ class GroupHelper:
     def __init__(self, app):
         self.app = app
 
-    def return_to_groups_page(self):
+    def open_groups_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("group page").click()
+        wd.find_element_by_link_text("groups").click()
 
     def create(self, group):
         wd = self.app.wd
@@ -26,6 +26,16 @@ class GroupHelper:
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
 
-    def open_groups_page(self):
+    def return_to_groups_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("groups").click()
+        wd.find_element_by_link_text("group page").click()
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+
+        self.return_to_groups_page()
